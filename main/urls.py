f88de818d26e25d61f4594e3a206fa63efea904a
riozulfandy <rio.zulfandy07@gmail.com>
2023-10-20 23:58:01 +0700

    Home Page Structure and Profile

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, login_user, signup, logout_user
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('signup/', signup, name='signup'), 
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,53 @@
+from main.models import Profile
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.urls import reverse
+from django.shortcuts import redirect
+from main.forms import SignUpForm, LoginForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
 def show_main(request):
-    context = {
-        'name': 'C-08',
-        'class': 'PBP C'
-    }
+    return render(request, "main.html")
 
-    return render(request, "main.html", context)
+def login_user(request):
+    form = LoginForm()
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user) # melakukan login terlebih dahulu
+            response = HttpResponseRedirect(reverse("main:show_main")) # membuat response
+            return response
+        else:
+            messages.info(request, 'Invalid username/password!')
+            
+    context = {'form':form}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = redirect('main:show_main')
+    return response
+
+def signup(request):
+    form = SignUpForm()
+
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            Profile.objects.create(
+                user = new_user,
+                status = new_user.status,
+                full_name = new_user.full_name,
+                email = new_user.email
+                )
+            messages.success(request, 'Sign up successful!')
+            return redirect('main:login')
+    
+    context = {'form':form}
+    return render(request, 'signup.html', context)
