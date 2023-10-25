714587b158c94c0275f5699d068153f942a79f2c
Alexander Audric Johansyah <audricjohansyah@gmail.com>
2023-10-25 22:53:13 +0700

    Finishing ordering function for Guest

--- main/models.py
+++ main/models.py
@@ -6,3 +6,13 @@ class Profile(models.Model):
     status = models.CharField(max_length=1)
     full_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=200)
+
+class Order(models.Model):
+    food_name = models.CharField(max_length=255)
+    food_price = models.DecimalField(max_digits=6, decimal_places=2)
+    amount = models.PositiveIntegerField(default=1)
+    order_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.customer.username} ordered {self.food_name} on {self.order_date}"
+
--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, login_user, signup, logout_user
+from main.views import show_main, login_user, signup, logout_user, food_view,drinks_view, order_food_ajax, show_order_guest, get_product_json, delete_order_ajax, edit_order_ajax,show_json,show_json_by_id, delete_allorder_ajax
 
 app_name = 'main'
 
@@ -8,4 +8,14 @@ urlpatterns = [
     path('signup/', signup, name='signup'), 
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('order-borrow/food', food_view, name='food_menu'),
+    path('order-borrow/drinks', drinks_view, name='drinks_menu'),
+    path('order-food/', order_food_ajax, name='order_food_ajax'),
+    path('show-order-guest/', show_order_guest, name="show_order_guest"),
+    path('get-product/', get_product_json, name='get_product_json'),
+    path('delete-order-ajax/<int:id>/', delete_order_ajax, name='delete_order_ajax'),
+    path('delete-allorder-ajax/', delete_allorder_ajax, name='delete_allorder_ajax'),
+    path('edit-order-ajax/<int:id>/', edit_order_ajax, name='edit_order_ajax'),
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
+    path('json/', show_json, name='show_json'),
 ]
