88a46b4e49cfcc2ab282ff6ca8ee2a906beee0c1
Alexander Audric Johansyah <audricjohansyah@gmail.com>
2023-10-29 00:42:30 +0700

    Integrating forms.py with models.py and AJAX

--- ordernborrow/forms.py
+++ ordernborrow/forms.py
@@ -1,10 +1,11 @@
 from django.forms import ModelForm
 from ordernborrow.models import *
+from django import forms
 
-class OrderForm(ModelForm):
+class OrderForm(forms.Form):
     class Meta:
         model = Order
-        fields = ["food_name", "food_price", "amount"]
+    amount = forms.IntegerField(label="Amount", widget=forms.TextInput(attrs={"class":"form-control","id":"amount", "name":"amount", "min":"1", "type":"number"}))
 
 class OrderMemberForm(ModelForm):
     class Meta:
